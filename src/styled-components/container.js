a31a529a7d4c0d03995d45ca9c6a13322a46989a
Prince-Sedem <sedemdev@gmail.com>
2025-06-04 18:42:33 +0100

    Update with an image

--- src/styled-components/container.js
+++ src/styled-components/container.js
@@ -5,8 +5,8 @@ import styled from "styled-components";
 //Home Page
 
 export const Container = styled.div`
-max-width: 850px; /* increased to accommodate 2 columns */
-height: 90vh;
+max-width: 650px; /* increased to accommodate 2 columns */
+height: 70vh;
 display: grid;
 background: #fff;
 border-radius: 15px;
@@ -14,9 +14,11 @@ margin-left: 10rem;
 grid-template-columns: 1fr 1fr; /* 👈 two equal columns */
 @media (max-width: 768px) {
     grid-template-columns: 1fr; /* Stack columns on smaller screens */
+    margin: 0;
   }
 `
 export const Input = styled.input`
+font-size: clamp(0.4rem, 1.5vw, 0.7rem);
 border-raduis: 4px;
 width: 225px;
 padding: 10px;
@@ -25,16 +27,16 @@ border: 1px solid gray;
 cursor: pointer;
 &::placeholder{font-style: italic; }
     @media (max-width: 768px) {
-    width: 300px;
+    width: 90%;
   };
 `
 export const Button = styled.button`
+font-size: clamp(0.4rem, 1.5vw, 0.7rem);
   padding: 10px;
-  margin: 10px 0;
   width: 250px;
+  margin-top: 10px;
+  margin-right: 55px;
   border-radius: 7px;
-  align-items: center;
-  justify-content: center;
   color: white;
   border: none;
   cursor: pointer;
@@ -46,6 +48,8 @@ export const Button = styled.button`
     @media (max-width: 768px) {
     width: 100%;
     font-size: 14px;
+    margin-right: 35px;
+    margin-top: 30px;
   }
 `
 
@@ -54,7 +58,8 @@ export const FormConatiner = styled.div`
 grid-column: 1;
 max-width: 500px;
 flex-direction: column;
-margin: auto 2rem;
+margin: 1rem;
+margin-left: 2rem;
   @media (max-width: 768px) {
     grid-row: 2;
     magrin: 0; /* 👈 Appears second on small screens */
@@ -73,36 +78,46 @@ padding: auto 10px;
 `
 export const Header = styled.p`
 font-weight: bold;
-font-size: clamp(2.5rem, 2.5vw, 4rem);
-line-height: 1.5;
+font-size: clamp(2rem, 1.5vw, 3rem);
+line-height: 1;
 color: '#0000';
 padding: 0 auto;
+
 `
 export const Text = styled.p`
-font-size: clamp(0.8rem, 2.5vw, 1rem);
-line-height: 1.5;
+font-size: clamp(0.4rem, 1.5vw, 0.7rem);
+line-height: 1;
 color: '#1d1918';
-padding: 0 auto;
+width: 280px;
+margin: 1rem;
+    @media (max-width: 768px) {
+    font-size: 14px;
+    
+  }
 `
 export const Image = styled.div`
 padding: 5px;
 margin: 20px;
-background-color: red;
+background-image: url('/images/web-hosting.png');
 background-repeat: no-repeat;
-max-width: 500px;
-height: 80vh;
-border-radius: 8px;
+background-position: cover;
+max-width: 300px;
+height: 62vh;
+border-radius: 15px;
   @media (max-width: 768px) {
     grid-row: 1; /* 👈 Appears first on small screens */
     width: 100%;
     padding: 0;
     margin: 0;
+    height: 40vh;
+    border-radius: 0 0 40px 40px;
   }
 `
 export const Row = styled.div`
 display: flex;
 align-items: center;
 gap: 20px;
+
 `
 export const ImageList = styled.div`
   background-image: url('/images/checked.png');
@@ -115,14 +130,29 @@ export const ImageList = styled.div`
   border-radius: 8px;
 `
 export const TextBox = styled.div`
-font-size: 1rem;
+font-size: clamp(0.4rem, 1.5vw, 0.7rem);
 color: #333;
 max-width: 500px;
+    @media (max-width: 768px) {
+    font-size: 14px;
+    
+  }
 `
 export const TextEmail = styled.div`
+font-size: clamp(0.4rem, 1.5vw, 0.6rem);
 font-weight: semi-bold;
 margin-right: 15rem;
 padding: 10px 0;
+    @media (max-width: 768px) {
+    margin-top: 10px;
+    font-size: 14px;
+    
+  }
+    @media (max-width: 480px) {
+    margin-top: 10px;
+    font-size: 12px;
+    
+  }
 `
 
 
@@ -157,8 +187,6 @@ margin-top: 20px;
 `
 export const SuccessText = styled.div`
 line-height: 1.5;
-
-
 `
 
 export const Span = styled.div`
