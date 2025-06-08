249078690b2bd73f1fc45abfbc096c50e05136eb
Prince-Sedem <sedemdev@gmail.com>
2025-06-08 13:37:49 +0100

    Update text section

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -8,7 +8,8 @@ function Success() {
             Thanks for subcribing!
         </SuccessHeader>
         <SuccessText>
-            A confirmation email has been sent to <Span> news@example.com</Span>.
+            A confirmation email has been sent to 
+            <Span> news@example.com</Span>.
             Please open it and click the button inside to confirm your subscription.
         </SuccessText>
         <SuccessButton>
