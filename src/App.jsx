4baacd4b515d23271173070fe0061ac86f37b302
Prince-Sedem <sedemdev@gmail.com>
2025-06-12 15:43:28 +0100

    Update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/Layout';
 import Success from './components/Success';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route path="/success" element={<Success />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './style.css';
@@ -6,7 +5,5 @@ import './style.css';
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-    <BrowserRouter>
         <App />
-    </BrowserRouter>
 );
