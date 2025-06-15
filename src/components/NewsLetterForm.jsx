d30a48263a0359721cfa0a9617b820eb20b376b9
Prince-Sedem <sedemdev@gmail.com>
2025-06-15 13:30:47 +0100

    Update file name

---
+++ src/components/NewsLetterForm.jsx
@@ -0,0 +1,68 @@
+import { useNavigate} from 'react-router-dom';
+import { useNewsLetterSubscription } from '../hooks/useNewsLetterSubscription';
+import { Container, Input, Button, FormConatiner, Header,Text, Image, Form, ImageList, TextBox, Row, TextEmail, ErorMessage, EmailSpan } from '../styled-components/container';
+
+const items = [
+  { image: '/images/checked.png', text: 'Product discovery and building what matters.' },
+  { image: '/images/checked.png', text: 'Measuring to ensure updates are a success.' },
+  { image: '/images/checked.png', text: 'And much more.' },
+];
+
+
+function NewsLetterForm() {
+
+    const navigate = useNavigate()
+
+
+    const {handleSubmit, error, email, setEmail, isValid} = useNewsLetterSubscription()
+    const handleFormSubmit = e => {
+        e.preventDefault()
+        const submission = handleSubmit(e) 
+        if (!isValid && submission) {
+            navigate("/success", {state:{email}})
+
+        }
+    }
+  return (
+    <Container>
+        <FormConatiner>
+            <Header>
+                Stay Updated!
+            </Header>
+            <Text>
+                Join 60,000+ product managers receiving monthly 
+                updates on.
+            </Text>
+                {items.map((item, index) => (
+                    <Row key={index}>
+                        <ImageList src={item.image} />
+                        <TextBox>
+                            <p>{item.text}</p>
+                        </TextBox>
+                    </Row>
+                ))}
+            <Form
+            onSubmit={handleFormSubmit}
+            >
+                <TextEmail>Email Address <EmailSpan>{error && <ErorMessage>{error}</ErorMessage>}</EmailSpan></TextEmail>    
+                <Input placeholder='enter your email'
+                error={!!error}
+                value={email}
+                onChange={({target})=> setEmail(target.value)}
+                />
+                    <Button 
+                    name='Submit'
+                    type='Submit'
+                    >
+                        Subcribe to monthly newsletter
+                    </Button>
+            </Form>
+        </FormConatiner>
+        <div>
+            <Image />
+        </div>
+    </Container>
+  )
+}
+
+export default NewsLetterForm
